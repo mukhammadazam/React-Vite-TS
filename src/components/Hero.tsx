e7c15b00ac68195fe29c6aa09e32107c35abf278
mukhammadazam <rizayevmuhammadazam9@gmail.com>
2023-06-16 17:27:44 +0500

    img adding

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,25 @@ import "./Hero.scss";
 const Hero = () => {
   return (
     <div className='hero'>
-      <span>NEW TREND</span>
+      <div className='hero-inner'>
+        <span className='hero-inner__leni text-center d-block text-sm-start'>
+          NEW TREND
+        </span>
+        <h1 className='hero-inner__title text-center text-sm-start'>
+          COLLUSION
+        </h1>
+        <p className='hero-inner__text text-center text-sm-start'>
+          An exclusive selection of this season's trends.
+        </p>
+        <div className='hero-inner__btnWpappe  d-flex justify-content-center '>
+          <button className='d-block hero-inner__btn1 border-0'>
+            DISCOVER
+          </button>
+          <button className='d-block hero-inner__btn1 ms-3 border-0'>
+            SHOP NOW
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
