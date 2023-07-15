62b9eaf07cddca7b70f98886e6a10925e3eeb251
mukhammadazam <rizayevmuhammadazam9@gmail.com>
2023-07-16 02:37:45 +0500

    mens add imgs

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,22 @@
-import logo from '../assets/LOGO.svg'
+import logo from "../assets/LOGO.svg";
 const Footer = () => {
   return (
     <div className='container'>
-<div className="footer">
-  
-</div>
+      <div className='footer'>
+        <div className=''>
+          <img src={logo} alt='logo' />
+          <p className='footer__text'>
+            We earned a reputation of being good at what we do. Let us take your
+            online shop to new dimension in success!
+          </p>
+        </div>
+        <div className=''>
+          <h4 className='footer__title'>RECIVE EMAIL UPDATES</h4>
+        
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
