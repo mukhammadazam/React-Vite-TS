e2af25f20be1a9f7a0f712d68a0e215a2e16d87d
mukhammadazam <rizayevmuhammadazam9@gmail.com>
2023-07-15 17:11:03 +0500

    Tachen start

--- src/components/Avone.tsx
+++ src/components/Avone.tsx
@@ -1,11 +1,93 @@
-import './Avone.scss'
+import "./Avone.scss";
+import data from "../db/style.json";
+import bg from "../assets/bgimg/bg.png";
+
+import { Link } from "react-router-dom";
 const Avone = () => {
   return (
     <div className='container'>
       <div className='avone'>
         <h3 className='text-center avone__title'>AVONE STYLES</h3>
         <p className='avone__text text-center'>Choose Your Favorite Color</p>
-        
+
+        <div className='section row'>
+          <div className='col-lg-6'>
+            {data.slice(0, 1).map((el) => (
+              <div className='pb-3 position-relative ' key={el.id}>
+                <Link
+                  target='__blank'
+                  to='https://www.instagram.com/'
+                  className='d-flex align-items-center  wrapper__img justify-content-center'>
+                  <img
+                    className='w-100 imgs'
+                    src={el.imgUrl}
+                    alt=''
+                    height={400}
+                  />
+                  <img className='position-absolute img' src={bg} alt='' />
+                </Link>
+              </div>
+            ))}
+            <div className='row pb-3 pb-lg-0'>
+              {data.slice(1, 2).map((el) => (
+                <div className='col-6 ' key={el.id}>
+                  <Link
+                    to='https://www.instagram.com/'
+                    className='d-flex align-items-center  wrapper__img justify-content-center'>
+                    <img
+                      className='w-100 card imgs'
+                      src={el.imgUrl}
+                      alt=''
+                      height={270}
+                    />
+                    <img className='position-absolute img' src={bg} alt='' />
+                  </Link>
+                </div>
+              ))}
+              {data.slice(2, 3).map((el) => (
+                <div className='col-6' key={el.id}>
+                  <Link
+                    to='https://www.instagram.com/'
+                    className='d-flex align-items-center  wrapper__img justify-content-center'>
+                    <img
+                      className='w-100 imgs rounded'
+                      src={el.imgUrl}
+                      alt=''
+                      height={270}
+                    />
+                    <img className='position-absolute img' src={bg} alt='' />
+                  </Link>
+                </div>
+              ))}
+            </div>
+          </div>
+
+          {data.slice(3, 4).map((el) => (
+            <div className='col-lg-6' key={el.id}>
+              <Link
+                to='https://www.instagram.com/'
+                className='d-flex align-items-center  wrapper__img justify-content-center'>
+                <img className='w-100 imgs' src={el.imgUrl} alt='' />
+                <img className='position-absolute img' src={bg} alt='' />
+              </Link>
+            </div>
+          ))}
+        </div>
+        {/* <div className='row pt-5'>
+          <div className='col-lg-6  col-12   pt-5'>
+            <img src={avone} alt='' width={552} height={602} />
+          </div>
+
+          <div className='col-lg-6 col-12   position-relative top-0 '>
+            <img className='position-absolute end-0' src={avone1} alt='' width={514} height={452} />
+            <img
+              className=' position-absolute end-0 bottom-0'
+              src={avone2}
+              alt='' width={227} height={281}
+            />
+            <Link to=''>SHOW WOMEN’S DRESS</Link>
+          </div>
+        </div> */}
       </div>
     </div>
   );
---
+++ src/components/Tachen.tsx
@@ -0,0 +1,12 @@
+import "./Tachen.scss";
+const Tachen = () => {
+  return (
+    <div className='tachen'>
+      <div className='tachen__text'>OWN THE DAY</div>
+      <div className='tachen__title'>TACHEN 19</div>
+      <button className='tachen__btn'>SHOP COLLECTION</button>
+    </div>
+  );
+};
+
+export default Tachen;
