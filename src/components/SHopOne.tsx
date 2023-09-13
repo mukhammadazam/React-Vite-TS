079013b87f300107ab517765df96d284ab19fdeb
mukhammadazam <rizayevmuhammadazam9@gmail.com>
2023-09-13 17:52:37 +0500

    ok

--- src/components/SHopOne.tsx
+++ src/components/SHopOne.tsx
@@ -1,7 +1,10 @@
+import heroImg from "../../public/shopImg/shop-hero.png";
+import logo1 from "../../public/shopImg/logo1.svg";
+import logo2 from "../../public/shopImg/logo2.svg";
+import logo3 from "../../public/shopImg/logo3.svg";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import "./ShopOne.scss";
-import heroImg from "../../public/shopImg/shop-hero.png";
 const SHopOne = () => {
   const [data, setData] = useState([]);
 
@@ -26,16 +29,50 @@ const SHopOne = () => {
     getFunction();
   }, []);
   return (
-    <div className='shop'>
-      <div className='container'>
-        <div className='shopOne d-lg-flex  align-items-lg-center justify-content-lg-around  '>
-          <h1 className='shopOne__title d-flex align-items-center justify-content-center text-center text-lg-start'>
-            Find it. Compare it. And pay how you like.
-          </h1>
-          <img className="d-block p-lg-0 pt-5 shopOne__img" src={heroImg} alt='' />
+    <>
+      <div className='shop'>
+        <div className='container'>
+          <div className='shopOne d-lg-flex  align-items-lg-center justify-content-lg-around  '>
+            <h1 className='shopOne__title d-flex align-items-center justify-content-center text-center text-lg-start'>
+              Find it. Compare it. And pay how you like.
+            </h1>
+            <img
+              className='d-block p-lg-0 pt-5 shopOne__img'
+              src={heroImg}
+              alt='heroimg'
+            />
+          </div>
         </div>
       </div>
-    </div>
+      <div className='container'>
+        <div className='row shopOne__row '>
+          <div className='col-lg-4  col-sm-4 pb-5 pb-sm-0  d-flex justify-content-center'>
+            <div className=''>
+              <img   className="d-block text-center" src={logo1} alt='logo' />
+              <strong className='shopOne__row--title '>
+                Shop what you love
+              </strong>
+            </div>
+          </div>
+          <div className='col-lg-4  col-sm-4 pb-5 pb-sm-0  d-flex justify-content-center'>
+            <div className=''>
+              <img   className="d-block text-center" src={logo3} alt='djfy' />
+              <strong className='shopOne__row--title '>
+                Shop what you love
+              </strong>
+            </div>
+          </div>
+          <div className='col-lg-4  col-sm-4   d-flex justify-content-center'>
+            <div className=''>
+              <img  className="d-block text-center"  src={logo3} alt='dsf' />
+              <strong className='shopOne__row--titler'>
+                Shop what you love
+              </strong>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
