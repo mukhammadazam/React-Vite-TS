d9b136ce919f4f512dbd12f5be065a393351c5a9
mukhammadazam <rizayevmuhammadazam9@gmail.com>
2023-09-18 15:39:49 +0500

    Ca

--- src/components/Corusel.tsx
+++ src/components/Corusel.tsx
@@ -1,5 +1,6 @@
 import Carousel from "react-bootstrap/Carousel";
 import { CarouselItem } from "react-bootstrap";
+import card from "../../public/shopImg/carta.png";
 import { Link } from "react-router-dom";
 import "./Corusel.scss";
 const Corusel = () => {
@@ -8,34 +9,49 @@ const Corusel = () => {
       <div className='container'>
         <Carousel>
           <CarouselItem>
-            <div className=''>
-              <span className='Card__leni d-block'>Klarna Card</span>
-              <p className='Card__title'>Pay anywhere that accepts Visa.</p>
-              <p className='Card__text'>
-                More time to pay is what we do. Charging interest? Not our
-                thing. The Klarna Card has a 0% APR. Your credit score won’t be
-                affected when you apply for or use the Klarna Card.
-              </p>
-              <Link className='d-block Card__link text-decoration-none ' to='/'>
-                Lear More
-              </Link>
+            <div className='d-md-flex align-items-md-center justify-content-md-around'>
+              <div className='d-flex justify-content-center'>
+                <div className=''>
+                  <span className='Card__leni d-block'>Klarna Card</span>
+                  <p className='Card__title'>Pay anywhere that accepts Visa.</p>
+                  <p className='Card__text'>
+                    More time to pay is what we do. Charging interest? Not our
+                    thing. The Klarna Card has a 0% APR. Your credit score won’t
+                    be affected when you apply for or use the Klarna Card.
+                  </p>
+                  <Link
+                    className='d-block Card__link text-decoration-none '
+                    to='/'>
+                    Lear More
+                  </Link>
+                </div>
+              </div>
+              <div className='d-flex justify-content-center'>
+                <img src={card} alt='card' />
+              </div>
             </div>
           </CarouselItem>
-          <CarouselItem className="row">
-            <div className='col-12 col-lg-6'>
-              <span className='Card__leni d-block'>Klarna Card</span>
-              <p className='Card__title'>Pay anywhere that accepts Visa.</p>
-              <p className='Card__text'>
-                More time to pay is what we do. Charging interest? Not our
-                thing. The Klarna Card has a 0% APR. Your credit score won’t be
-                affected when you apply for or use the Klarna Card.
-              </p>
-              <Link className='d-block Card__link text-decoration-none ' to='/'>
-                Lear More
-              </Link>
-            </div>
-            <div className="col-12 col-lg-6">
-
+          <CarouselItem>
+            <div className='d-md-flex align-items-md-center justify-content-md-around'>
+              <div className='d-flex justify-content-center'>
+                <div className=''>
+                  <span className='Card__leni d-block'>Klarna Card</span>
+                  <p className='Card__title'>Pay anywhere that accepts Visa.</p>
+                  <p className='Card__text'>
+                    More time to pay is what we do. Charging interest? Not our
+                    thing. The Klarna Card has a 0% APR. Your credit score won’t
+                    be affected when you apply for or use the Klarna Card.
+                  </p>
+                  <Link
+                    className='d-block Card__link text-decoration-none '
+                    to='/'>
+                    Lear More
+                  </Link>
+                </div>
+              </div>
+              <div className='d-flex justify-content-center'>
+                <img src={card} alt='card' />
+              </div>
             </div>
           </CarouselItem>
         </Carousel>
